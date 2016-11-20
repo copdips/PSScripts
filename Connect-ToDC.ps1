4f0135d180d51fc76e71f90d59bd3b479807e530
Xiang ZHU <xiang.zhu@outlook.com>
2016-11-20 13:19:49 +0100

    automatiser credntial creation

--- Connect-ToDC.ps1
+++ Connect-ToDC.ps1
@@ -1,8 +1,19 @@
-﻿$cred = Get-Credential contoso\administrator
+﻿# $credContoso = Get-Credential contoso\administrator
+
+# $credContosoFile = 'D:\xiang\Dropbox\Certification\Microsoft\Powershell\credContoso.txt'
+
+# read-host -assecurestring | convertfrom-securestring | out-file $credContosoFile
+
+$userContoso = 'contoso\administrator'
+
+# $passwordContoso = 'Password1' | convertto-securestring -AsPlainText -Force 
+
+$passwordContoso = get-content $credContosoFile | convertto-securestring
+
+$credContoso = new-object -typename System.Management.Automation.PSCredential -argumentlist $userContoso,$passwordContoso
 
 $PSSessionOption = New-PSSessionOption -SkipCACheck -SkipCNCheck -SkipRevocationCheck
 
-Enter-PSSession 12R201.contoso.com -Credential $cred -UseSSL -SessionOption $PSSessionOption
+Enter-PSSession 12R201.contoso.com -Credential $credContoso -UseSSL -SessionOption $PSSessionOption
 
 sl c:\temp
-
