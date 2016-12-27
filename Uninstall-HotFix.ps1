38c95704e1c8d51a83b8983e78373d1d39cd937d
Xiang ZHU <xiang.zhu@outlook.com>
2016-12-27 10:26:16 +0100

    add comments

--- Uninstall-HotFix.ps1
+++ Uninstall-HotFix.ps1
@@ -1,36 +1,49 @@
-# http://techibee.com/powershell/powershell-uninstall-windows-hotfixesupdates/1084
 
-function Uninstall-HotFix {
+# It's better to use this comdlet when there's no other wusa process is also working on the computer
+
+function Uninstall-HotFix
+{
     [cmdletbinding()]
-    param(
+    param
+    (
         $computername = $env:COMPUTERNAME,
         [string] $HotFixID
     )            
 
     $hotFix = Get-WmiObject -ComputerName $computername -Class win32_quickfixengineering | Where-Object {$_.HotFixId -match $HotFixID}    
 
-
     if($hotFix) 
     {
         $hotFixNumber = $HotfixID.Replace("KB","")
+        Write-Host "$computername : Found the hotfix $HotFixID , and uninstalling it"
 
-        Write-Host "$computername : Found the hotfix $HotFixID ,and uninstalling it"
+        #No more use WMICLASS to start the process on remote computer
         # $UninstallString = "cmd.exe /c wusa.exe /uninstall /KB:$Using:hotFixNumber /quiet /norestart"
-        # ([WMICLASS]"\\$computername\ROOT\CIMV2:win32_process").Create($UninstallString) | out-null
+        #([WMICLASS]"\\$computername\ROOT\CIMV2:win32_process").Create($UninstallString) | out-null
         Invoke-Command -cn $computername {cmd.exe /c wusa.exe /uninstall /KB:$Using:hotFixNumber /quiet /norestart}
 
-
-        # Bug if there's other wusa ongoing
-        while (@(Get-Process wusa -computername $computername -ErrorAction SilentlyContinue).Count -ne 0) {
+        # Bug if there's other wusa is ongoing, and is taking much time 
+        while (@(Get-Process wusa -computername $computername -ErrorAction SilentlyContinue).Count -ne 0) 
+        {
             Start-Sleep 3
             Write-Host "Waiting for $HotFixID removal to finish ..."
         }
         write-host "$computername : Completed the uninstallation of $hotfixID"
+	
+        $uninstallResult = Get-WmiObject -ComputerName $computername -Class win32_quickfixengineering | Where-Object {$_.HotFixId -match $HotFixID}
+
+        if (-not $uninstallResult) 
+        {
+            write-host "$computername : No more hotfix $hotfixID found"
+        }    
+        else 
+        {
+            write-host "$computername : Still found hotfix $hotfixID" -F Red
+        }
     }
-    
     else
     {         
-        write-host "$computername : Given hotfix $hotfixID not found"
+        write-host "$computername : Given hotfix $hotfixID not found" -F Red
     }            
 
 }
