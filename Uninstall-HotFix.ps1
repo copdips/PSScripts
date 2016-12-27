0c19fced2fb34c11b4fb3a0d775224495dc9f9bb
Xiang ZHU <xiang.zhu@outlook.com>
2016-12-27 10:18:16 +0100

    use Invoke-Command to start the uninstall remotely

--- Uninstall-HotFix.ps1
+++ Uninstall-HotFix.ps1
@@ -7,15 +7,16 @@ function Uninstall-HotFix {
         [string] $HotFixID
     )            
 
-    $hotFix = Get-WmiObject win32_quickfixengineering | ? {$_.HotFixId -match $HotFixID}    
+    $hotFix = Get-WmiObject win32_quickfixengineering | Where-Object {$_.HotFixId -match $HotFixID}    
 
     if($hotFix) 
     {
         $hotFixNumber = $HotfixID.Replace("KB","")
         Write-Host "$computername : Found the hotfix $HotFixID ,and uninstalling it"
-        $UninstallString = "cmd.exe /c wusa.exe /uninstall /KB:$hotFixNumber /quiet /norestart"
+        # $UninstallString = "cmd.exe /c wusa.exe /uninstall /KB:$Using:hotFixNumber /quiet /norestart"
         # ([WMICLASS]"\\$computername\ROOT\CIMV2:win32_process").Create($UninstallString) | out-null
-        Invoke-Expression $UninstallString
+        Invoke-Command -cn $computername {cmd.exe /c wusa.exe /uninstall /KB:$Using:hotFixNumber /quiet /norestart}
+
 
         # Bug if there's other wusa ongoing
         while (@(Get-Process wusa -computername $computername -ErrorAction SilentlyContinue).Count -ne 0) {
