2f74d893907ab37e5b5a86bb5ffd533e99727dc6
Xiang ZHU <xiang.zhu@outlook.com>
2017-03-07 23:10:56 +0100

    replace mkdir of Dos by new-item of Powershell

--- New-PowershellModule.ps1
+++ New-PowershellModule.ps1
@@ -5,13 +5,13 @@ $Description = "$ModuleName PowerShell module"
 $PowerShellVersion = $PSVersionTable.PSVersion.ToString()
 
 # Create the module and private function directories
-mkdir $Path\$ModuleName
-mkdir $Path\$ModuleName\Private
-mkdir $Path\$ModuleName\Public
-mkdir $Path\$ModuleName\Lib
-mkdir $Path\$ModuleName\Bin
-mkdir $Path\$ModuleName\en-US # For about_Help files
-mkdir $Path\Tests
+New-Item "$Path\$ModuleName" -ItemType Directory
+New-Item "$Path\$ModuleName\Private" -ItemType Directory
+New-Item "$Path\$ModuleName\Public" -ItemType Directory
+New-Item "$Path\$ModuleName\Lib" -ItemType Directory
+New-Item "$Path\$ModuleName\Bin" -ItemType Directory
+New-Item "$Path\$ModuleName\en-US" -ItemType Directory # For about_Help files
+New-Item "$Path\Tests" -ItemType Directory
 
 #Create the module and related files
 New-Item "$Path\$ModuleName\$ModuleName.psm1" -ItemType File
@@ -23,4 +23,4 @@ New-ModuleManifest -Path $Path\$ModuleName\$ModuleName.psd1 `
                    -Description $Description `
                    -PowerShellVersion $PowerShellVersion `
                    -Author $Author `
--FormatsToProcess "$ModuleName.Format.ps1xml"
+                   -FormatsToProcess "$ModuleName.Format.ps1xml"
